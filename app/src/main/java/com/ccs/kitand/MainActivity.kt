bd93cbe0492168d99962f92db0c7bd59a37ee999
Graeme Costin <graeme_costin@wycliffe.org.au>
2020-08-05 22:22:05 +1000

    Added Android View Binding to the (still very simple) Main layout.

--- app/src/main/java/com/ccs/kitand/MainActivity.kt
+++ app/src/main/java/com/ccs/kitand/MainActivity.kt
@@ -2,24 +2,34 @@ package com.ccs.kitand
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ccs.kitand.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         println("onCreate()")
     }
 
     override fun onStart() {
         super.onStart()
+        binding.greeting.text = "onStart()"
         println("onStart()")
     }
 
     override fun onResume() {
         super.onResume()
+        binding.greeting.text = "onResume()"
         println("onResume()")
+        binding.greeting.text = KITApp.greeting
         println(KITApp.greeting)
         KITApp.greeting = "Here is the new greeting"
+        binding.greeting.text = KITApp.greeting
         println(KITApp.greeting)
     }
 }
